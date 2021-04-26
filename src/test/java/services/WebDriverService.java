37299bd525b39db2d32fc815e4b58c6b1f4d6610
Čamaj Vladimír <vladimir.camaj@gmail.com>
2021-04-26 21:03:02 +0200

    Gecko driver update

--- src/test/java/services/WebDriverService.java
+++ src/test/java/services/WebDriverService.java
@@ -43,7 +43,7 @@ public class WebDriverService
         FirefoxOptions options = new FirefoxOptions();
         options.setCapability("marionette",true);
         options.setHeadless(conf.getBoolean("env.production"));
-        options.addArguments("--version");
+        options.addArguments("-version");
 
         Configuration.remote = HUB_URL;
         //Configuration.browser = browser;
