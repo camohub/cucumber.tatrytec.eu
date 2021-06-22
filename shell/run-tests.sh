59ef9eccdb52423c980e1f0c6faa3cef05353dad
Čamaj Vladimír <vladimir.camaj@gmail.com>
2021-06-22 09:08:52 +0200

    run-tests.sh - whoami

--- shell/run-tests.sh
+++ shell/run-tests.sh
@@ -4,6 +4,8 @@
 
 mvn clean compile test surefire-report:report-only  -B -P${testCategory} -DBUILD_URL=${BUILD_URL} -Dbrowser=${browser}
 
+echo whoami
+
 echo "[INFO] ------------------------------------------------------------------------"
 echo "[INFO]  All tests are done"
 echo "[INFO] ------------------------------------------------------------------------"
